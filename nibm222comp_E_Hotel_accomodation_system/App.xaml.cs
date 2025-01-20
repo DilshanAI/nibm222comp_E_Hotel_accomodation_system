865a17ec154ae8901421e7eb596d9fc39469c3e4
DilshanAI <aaidamarasinghe222p004@gmail.com>
2025-01-20 23:57:05 +0530

    Add project files.

---
+++ nibm222comp_E_Hotel_accomodation_system/App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace nibm222comp_E_Hotel_accomodation_system
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
---
+++ nibm222comp_E_Hotel_accomodation_system/MainWindow.xaml.cs
@@ -0,0 +1,24 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace nibm222comp_E_Hotel_accomodation_system
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
