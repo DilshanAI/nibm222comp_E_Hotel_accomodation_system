231801260f1c4ae5d677b54b19c071e6a40de5ad
DilshanAI <aaidamarasinghe222p004@gmail.com>
2025-01-21 23:10:38 +0530

    NEHAS -1 -User Athentication

---
+++ nibm222comp_E_Hotel_accomodation_system/Connection.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Data.SqlClient;
+
+namespace nibm222comp_E_Hotel_accomodation_system
+{
+    class Connection
+    {
+        private SqlConnection con;
+        private SqlCommand cmd;
+        private SqlDataAdapter da;
+
+
+        public static readonly string ConnectionString =
+    @"Data Source=DESKTOP-KG96SMV;Initial Catalog=HotelAccomodationSystem;Integrated Security=True;TrustServerCertificate=True";
+
+
+        public Connection() // Default Constructor
+        {
+            con = new SqlConnection(ConnectionString);
+        }
+        public void openConnection()
+        {
+            con.Open();
+        }
+        public void closeConnection()
+        {
+            con.Close();
+        }
+
+        public DataTable getData(string a)
+        {
+            openConnection();
+            da = new SqlDataAdapter(a, con);
+            DataTable dt = new DataTable();
+            da.Fill(dt);
+            closeConnection();
+            return dt;
+        }
+        public void executeQuery(string query)
+        {
+            try
+            {
+                openConnection();
+                cmd = new SqlCommand(query, con);
+                cmd.ExecuteNonQuery();
+            }
+            finally
+            {
+                closeConnection();
+            }
+        }
+
+    }
+}
--- nibm222comp_E_Hotel_accomodation_system/MainWindow.xaml.cs
+++ nibm222comp_E_Hotel_accomodation_system/MainWindow.xaml.cs
@@ -8,6 +8,8 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using Microsoft.Data.SqlClient;
+using System.Text.RegularExpressions;
 
 namespace nibm222comp_E_Hotel_accomodation_system
 {
@@ -16,9 +18,64 @@ namespace nibm222comp_E_Hotel_accomodation_system
     /// </summary>
     public partial class MainWindow : Window
     {
+        SqlConnection sqlcon = new SqlConnection(Connection.ConnectionString);
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private void Username_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            string username = Username.Text;
+            string password = Password.Password;
+
+            if (IsValidPassword(password))
+            {
+                try
+                {
+                    sqlcon.Open();
+                    string query = "SELECT COUNT(1) FROM Login WHERE Username=@Username AND Password=@Password";
+                    SqlCommand sqlCmd = new SqlCommand(query, sqlcon);
+                    sqlCmd.Parameters.AddWithValue("@Username", username);
+                    sqlCmd.Parameters.AddWithValue("@Password", password);
+                    int count = Convert.ToInt32(sqlCmd.ExecuteScalar());
+
+                    if (count == 1)
+                    {
+
+                        MessageBox.Show("Login successful!");
+                   
+                       
+                    }
+                    else
+                    {
+                        MessageBox.Show("Username or password is incorrect.");
+                    }
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show(ex.Message);
+                }
+                finally
+                {
+                    sqlcon.Close();
+                }
+            }
+            else
+            {
+                MessageBox.Show("Invalid email format or password length.");
+            }
+        }
+  
+
+        private bool IsValidPassword(string password)
+        {
+            return password.Length > 7;
         }
     }
 }
